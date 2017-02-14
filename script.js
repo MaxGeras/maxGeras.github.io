bcaf69a9b2b0d88844b6504647731343682a1c34
MaxGeras <ger14009@byui.edu>
2017-02-13 18:36:25 -0700

    local Storage

--- script.js
+++ script.js
@@ -83,5 +83,6 @@ function myFunction(myJson){
             + myJson.students[i].gpa + "\n";  
        i++; 
     }
-    document.getElementById("output").innerHTML = str;    
+    localStorage.setItem("input", "str");
+    document.getElementById("output").innerHTML = localStorage.getItem("input");    
 }
